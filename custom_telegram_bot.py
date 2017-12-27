b9f832a6dec5e4b90fe0d148420fc95c3d6c4fbe
Janghyeon Lee <k11tos@nate.com>
2017-12-27 21:47:53 +0900

    update

--- custom_telegram_bot.py
+++ custom_telegram_bot.py
@@ -16,6 +16,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import os
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
 
 # Enable logging
